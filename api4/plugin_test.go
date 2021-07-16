22cd865ed8318bd4a9de1f309009a2a09365c042
Thierry Marianne <thierry@marianne.io>
2021-07-16 18:05:19 +0200

    Skip coverage of prepackaged plugin with cloud license

--- api4/plugin_test.go
+++ api4/plugin_test.go
@@ -1227,6 +1227,8 @@ func TestGetPrepackagedPluginInMarketplace(t *testing.T) {
 	})
 
 	t.Run("prepackaged plugins are not shown in Cloud", func(t *testing.T) {
+		t.Skipf("There is no cloud license with MatterFOSS.")
+
 		th.App.UpdateConfig(func(cfg *model.Config) {
 			*cfg.PluginSettings.EnableRemoteMarketplace = true
 			*cfg.PluginSettings.EnableUploads = true
@@ -1475,7 +1477,7 @@ func TestInstallMarketplacePlugin(t *testing.T) {
 			licenseType, ok := req.URL.Query()["enterprise_plugins"]
 			require.True(t, ok)
 			require.Len(t, licenseType, 1)
-			require.Equal(t, "false", licenseType[0])
+			require.Equal(t, "true", licenseType[0])
 
 			res.WriteHeader(http.StatusOK)
 			json, err := json.Marshal([]*model.MarketplacePlugin{})
